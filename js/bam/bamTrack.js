181e2bb6f82cbdfeb25bd9b754db8bcf4d32d8c6
Douglass Turner <douglass.turner@gmail.com>
2016-09-22 17:28:04 -0400

    referenceFrame.chr -> referenceFrame.chrName

--- js/bam/bamTrack.js
+++ js/bam/bamTrack.js
@@ -457,7 +457,7 @@ var igv = (function (igv) {
         if (coverage) {
 
 
-            nameValues.push(igv.browser.referenceFrame.chr + ":" + igv.numberFormatter(1 + genomicLocation));
+            nameValues.push(igv.browser.referenceFrame.chrName + ":" + igv.numberFormatter(1 + genomicLocation));
 
             nameValues.push({name: 'Total Count', value: coverage.total});
 
